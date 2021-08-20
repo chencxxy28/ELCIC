36644419f0f414327b9e8a8c928d16afb5a45266
chencxxy28 <chencxxy28@gmail.com>
2021-08-20 11:31:15 -0400

    Update test_modelselection_geenomissing.R

--- tests/testthat/test_modelselection_geenomissing.R
+++ tests/testthat/test_modelselection_geenomissing.R
@@ -1,5 +1,7 @@
 data("geetoydata")
 
+x=geetoydata$x
+
 test_that("output error: y is not a vector",{expect_error(ELCIC.gee(x=geetoydata$x,y=as.data.frame(geetoydata$y),r=rep(1,nrow(x)),id=geetoydata$id,time=3,index.var=c(1,2,3),name.var=NULL,dist="poisson",corstr="exchangeable"),"y should be in a vector format")})
 
 test_that("output error: x is not a matrix",{expect_error(ELCIC.gee(x=as.data.frame(geetoydata$x),y=(geetoydata$y),r=rep(1,nrow(x)),id=geetoydata$id,time=3,index.var=c(1,2,3),name.var=NULL,dist="poisson",corstr="exchangeable"),"x should be in a matrix format")})
@@ -14,3 +16,11 @@ test_that("output error: undefined candidate model",{expect_error(ELCIC.gee(x=(g
 test_that("output error: non-unique variable name",{expect_error(ELCIC.gee(x=(geetoydata$x),y=(geetoydata$y),r=rep(1,nrow(x)),id=geetoydata$id,time=3,index.var=NULL,name.var=c("intercept","x1","x1"),dist="poisson",corstr="exchangeable"),"Invalid candidate model provided")})
 
 test_that("output error: invalid correlation structure for outcomes in gee without missing",{expect_error(ELCIC.gee(x=(geetoydata$x),y=(geetoydata$y),r=rep(1,nrow(x)),id=geetoydata$id,time=3,index.var=NULL,name.var=c("intercept","x1","x2"),dist="poisson",corstr="ar2"),"Invalid type of correlation structure for outcomes. It should be one of ar1,exchangeable,independence")})
+
+output1<-ELCIC.gee(x=(geetoydata$x),y=(geetoydata$y),r=rep(1,nrow(x)),id=geetoydata$id,time=3,index.var=NULL,name.var=c("intercept","x1","x2"),dist="poisson",corstr="ar1",joint=TRUE)
+output2<-ELCIC.gee(x=(geetoydata$x),y=(geetoydata$y),r=rep(1,nrow(x)),id=geetoydata$id,time=3,index.var=c(1,2,3),name.var=c("intercept","x1","x2"),dist="poisson",corstr="ar1",joint=TRUE)
+test_that("output error: invalid correlation structure for outcomes in gee without missing",{expect_equal(output1,output2)})
+
+
+
+
