a4e8f98d3b33ebb8c07dfda909ec79ae78b5e001
chencxxy28 <chencxxy28@gmail.com>
2021-08-22 13:02:49 -0400

    Update test_datageneration.R

--- tests/testthat/test_datageneration.R
+++ tests/testthat/test_datageneration.R
@@ -9,4 +9,3 @@ time=3
 geetoydata<-gee.generator(beta=c(-1,1,0.5,0),samplesize=samplesize,time=time,num.time.dep=2,num.time.indep=1,rho=0.4,x.rho=0.2,dist="gaussian",cor.str="exchangeable",x.cor.str="exchangeable")
 test_that("output equal: same length",{expect_equal(length(geetoydata),3)})
 
-
