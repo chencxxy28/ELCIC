0b073f8d28a0f1de40a49f03a4f02660a6cfbcef
chencxxy28 <chencxxy28@gmail.com>
2021-08-22 15:53:09 -0400

    Update test_modelselection_mlic.R

--- tests/testthat/test_modelselection_mlic.R
+++ tests/testthat/test_modelselection_mlic.R
@@ -9,6 +9,13 @@ test_that("output equal: same output given both index and var.names, given joint
 
 
 output1<-MLIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=list(c(1,2,3)),name.var.sets=NULL,dist="binomial",candidate.cor.sets="exchangeable",joints=TRUE)
+output2<-MLIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=list(c(1,2,3)),name.var.sets=NULL,dist="binomial",candidate.cor.sets="exchangeable",joints=TRUE)
+test_that("output equal: same output given both index and var.names, given joints=true",{expect_equal(output1,output2)})
+
+
+output1<-MLIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
                              ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=NULL,name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
 output2<-MLIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
                              ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
@@ -20,3 +27,10 @@ output1<-MLIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
 output2<-MLIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
                              ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=NULL,name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets=c("ar1","exchangeable"),joints=FALSE)
 test_that("output equal: same output given multiple correlation structures, given joints=false",{expect_equal(output1,output2)})
+
+
+output1<-MLIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=list(c(1,2,3)),name.var.sets=NULL,dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
+output2<-MLIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=list(c(1,2,3)),name.var.sets=NULL,dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
+test_that("output equal: same output given both index and var.names, given joints=false",{expect_equal(output1,output2)})
