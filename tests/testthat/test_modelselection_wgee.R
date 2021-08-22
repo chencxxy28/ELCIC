f3857e6444a25622dd10b625f25b5b1262c4d905
chencxxy28 <chencxxy28@gmail.com>
2021-08-22 12:03:48 -0400

    Update test_modelselection_wgee.R

--- tests/testthat/test_modelselection_wgee.R
+++ tests/testthat/test_modelselection_wgee.R
@@ -58,5 +58,38 @@ output2<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
 test_that("output equal: same output given multiple correlation structures, given joints=false",{expect_equal(output1,output2)})
 
+#try different distributions
+set.seed(515413)
+library(wgeesel)
+n=200
+time=3
+rho=0.5
+id=rep(1:n,each=time)
+truecorstr<-"exchangeable" #"ar1","exchangeable"
+truedist<-"poisson" #"binary","poisson"
+xf <- cbind(1, rep(runif(n), each=time), rep(0:(time-1),times=n), rnorm(time*n))
+xT<-xf[,1:3]
+betaT=c(-1,1,0.4)
+x_mis=cbind(matrix(1,nrow=length(id),ncol=1),runif(n*time,-0.5,0.5))
+para=c(1.74,0.5,-0.8)
+data_gernated<-data_sim(id,rho,phi,xT,betaT,x_mis,para,truecorstr,truedist,lag_level = 1)
 
+x<-xf
+colnames(x)<-c("intercept","x1","x2","x3")
+x_mis<-as.matrix(data.frame(intercept=data_gernated$data$V1,x_mis1=data_gernated$data$V2,x_mis2=data_gernated$data$ylag1))
+wgeetoydata<-list(y=data_gernated$data$response_mis,x=x,x_mis=x_mis,id=id,obs_ind=data_gernated$data$ind)
+
+
+output1<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+                              ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="poisson",candidate.cor.sets=c("exchangeable","ar1"),joints=FALSE)
+output2<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+                              ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="poisson",candidate.cor.sets="exchangeable",joints=FALSE)
+test_that("output equal: same output given multiple correlation structures, given joints=false",{expect_equal(output1,output2)})
+
+
+output1<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+                              ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="gaussian",candidate.cor.sets=c("exchangeable","ar1"),joints=FALSE)
+output2<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+                              ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="gaussian",candidate.cor.sets="exchangeable",joints=FALSE)
+test_that("output equal: same output given multiple correlation structures, given joints=false",{expect_equal(output1,output2)})
 
