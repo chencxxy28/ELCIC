bbe99ba8854e8333c1a1271539efebede88adbcc
chencxxy28 <chencxxy28@gmail.com>
2021-08-22 20:21:52 -0400

    Update test_modelselection_wgee.R

--- tests/testthat/test_modelselection_wgee.R
+++ tests/testthat/test_modelselection_wgee.R
@@ -1,82 +1,82 @@
 data("wgeetoydata")
 
-test_that("output error: y is not a vector",{expect_error(ELCIC.wgee(x=wgeetoydata$x,y=data.frame(wgeetoydata$y)
+test_that("output error: y is not a vector",{expect_error(ELCIC.wgee.single(x=wgeetoydata$x,y=data.frame(wgeetoydata$y)
                                                                      ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,index.var=c(1,2,3),name.var=NULL,dist="binomial",corstr="exchangeable",joints=T),"y should be in a vector format")})
 
-test_that("output error: x is not a matrix",{expect_error(ELCIC.wgee(x=data.frame(wgeetoydata$x),y=(wgeetoydata$y)
+test_that("output error: x is not a matrix",{expect_error(ELCIC.wgee.single(x=data.frame(wgeetoydata$x),y=(wgeetoydata$y)
                                                                      ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,index.var=c(1,2,3),name.var=NULL,dist="binomial",corstr="exchangeable",joints=T),"x should be in a matrix format")})
 
-test_that("output error: x_mis is not a matrix",{expect_error(ELCIC.wgee(x=(wgeetoydata$x),y=(wgeetoydata$y)
+test_that("output error: x_mis is not a matrix",{expect_error(ELCIC.wgee.single(x=(wgeetoydata$x),y=(wgeetoydata$y)
                                                                      ,x_mis=data.frame(wgeetoydata$x_mis),r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,index.var=c(1,2,3),name.var=NULL,dist="binomial",corstr="exchangeable",joints=T),"x_mis should be in a matrix format")})
 
-test_that("output error: dist is not undefined",{expect_error(ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
+test_that("output error: dist is not undefined",{expect_error(ELCIC.wgee.single(x=wgeetoydata$x,y=(wgeetoydata$y)
                                                                          ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,index.var=c(1,2,3),name.var=NULL,dist="gamma",corstr="exchangeable",joints=T),"Invalid type of dist. It should be one of gaussian,binomial,poisson")})
 
 
-test_that("output error: non-unique index",{expect_error(ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
+test_that("output error: non-unique index",{expect_error(ELCIC.wgee.single(x=wgeetoydata$x,y=(wgeetoydata$y)
                                                                     ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,index.var=c(1,1,3),name.var=NULL,dist="binomial",corstr="exchangeable",joints=T),"Invalid candidate model provided")})
 
-test_that("output error: undefined candidate model",{expect_error(ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
+test_that("output error: undefined candidate model",{expect_error(ELCIC.wgee.single(x=wgeetoydata$x,y=(wgeetoydata$y)
                                                                              ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,index.var=NULL,name.var=c("intercept","x1","x2","x3","x4"),dist="binomial",corstr="exchangeable",joints=T),"Invalid candidate model provided")})
 
-test_that("output error: non-unique variable name",{expect_error(ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
+test_that("output error: non-unique variable name",{expect_error(ELCIC.wgee.single(x=wgeetoydata$x,y=(wgeetoydata$y)
                                                                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,index.var=NULL,name.var=c("intercept","x1","x1"),dist="binomial",corstr="exchangeable",joints=T),"Invalid candidate model provided")})
 
-test_that("output error: invalid correlation structure for outcomes in gee without missing",{expect_error(ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
+test_that("output error: invalid correlation structure for outcomes in gee without missing",{expect_error(ELCIC.wgee.single(x=wgeetoydata$x,y=(wgeetoydata$y)
                                                                                                                      ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,index.var=NULL,name.var=c("intercept","x1","x2"),dist="binomial",corstr="ar2",joints=T),"Invalid type of correlation structure for outcomes. It should be one of ar1,exchangeable,independence")})
 
-output1<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
+output1<-ELCIC.wgee.single(x=wgeetoydata$x,y=(wgeetoydata$y)
                     ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,index.var=NULL,name.var=c("intercept","x1","x2"),dist="binomial",corstr="exchangeable",joints=T)
-output2<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
+output2<-ELCIC.wgee.single(x=wgeetoydata$x,y=(wgeetoydata$y)
                     ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,index.var=c(1,2,3),name.var=c("intercept","x1","x2"),dist="binomial",corstr="exchangeable",joints=T)
 test_that("output equal: same output given both index and var.names, given joints=true",{expect_equal(output1,output2)})
 
 
-output1<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
+output1<-ELCIC.wgee.single(x=wgeetoydata$x,y=(wgeetoydata$y)
                     ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,index.var=NULL,name.var=c("intercept","x1","x2"),dist="binomial",corstr="exchangeable",joints=FALSE)
-output2<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
+output2<-ELCIC.wgee.single(x=wgeetoydata$x,y=(wgeetoydata$y)
                     ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,index.var=c(1,2,3),name.var=c("intercept","x1","x2"),dist="binomial",corstr="exchangeable",joints=FALSE)
 test_that("output equal: same output given both index and var.names, given joints=false",{expect_equal(output1,output2)})
 
 
-output1<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output1<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                     ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=NULL,name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=TRUE)
-output2<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output2<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                     ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=TRUE)
 test_that("output equal: same output given both index and var.names, given joints=true",{expect_equal(output1,output2)})
 
 
-output1<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output1<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),dist="binomial",candidate.cor.sets="exchangeable",joints=TRUE)
-output2<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output2<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),dist="binomial",candidate.cor.sets="exchangeable",joints=TRUE)
 test_that("output equal: same output given both index and var.names, given joints=false",{expect_equal(output1,output2)})
 
 
-output1<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output1<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=NULL,name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
-output2<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output2<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
 test_that("output equal: same output given both index and var.names, given joints=false",{expect_equal(output1,output2)})
 
 
-output1<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output1<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
-output2<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output2<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
 test_that("output equal: same output given both index and var.names, given joints=false",{expect_equal(output1,output2)})
 
 
-output1<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output1<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets=c("exchangeable","ar1"),joints=FALSE)
-output2<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output2<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
 test_that("output equal: same output given multiple correlation structures, given joints=false",{expect_equal(output1,output2)})
 
 
-output1<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output1<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets=c("independence","ar1"),joints=FALSE)
-output2<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output2<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="independence",joints=FALSE)
 test_that("output equal: same output given multiple correlation structures, given joints=false",{expect_equal(output1,output2)})
 
@@ -102,16 +102,16 @@ x_mis<-as.matrix(data.frame(intercept=data_gernated$data$V1,x_mis1=data_gernated
 wgeetoydata<-list(y=data_gernated$data$response_mis,x=x,x_mis=x_mis,id=id,obs_ind=data_gernated$data$ind)
 
 
-output1<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output1<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="poisson",candidate.cor.sets=c("exchangeable","ar1"),joints=FALSE)
-output2<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output2<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="poisson",candidate.cor.sets="exchangeable",joints=FALSE)
 test_that("output equal: same output given multiple correlation structures, given joints=false",{expect_equal(output1,output2)})
 
 
-output1<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output1<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="gaussian",candidate.cor.sets=c("exchangeable","ar1"),joints=FALSE)
-output2<-ELCIC.wgee.procedure(x=wgeetoydata$x,y=(wgeetoydata$y)
+output2<-ELCIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
                               ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,time=3,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="gaussian",candidate.cor.sets="exchangeable",joints=FALSE)
 test_that("output equal: same output given multiple correlation structures, given joints=false",{expect_equal(output1,output2)})
 
