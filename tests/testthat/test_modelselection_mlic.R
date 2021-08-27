ac8ff539b53159332bced73582cdf004a17c45f0
chencxxy28 <chencxxy28@gmail.com>
2021-08-27 15:52:02 -0400

    Update test_modelselection_mlic.R

--- tests/testthat/test_modelselection_mlic.R
+++ tests/testthat/test_modelselection_mlic.R
@@ -1,36 +1,36 @@
-data("wgeetoydata")
+data("wgeesimdata")
 
 
-output1<-MLIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
-                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=NULL,name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=TRUE)
-output2<-MLIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
-                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=TRUE)
+output1<-MLIC.wgee(x=wgeesimdata$x,y=(wgeesimdata$y)
+                             ,x_mis=wgeesimdata$x_mis,r=wgeesimdata$obs_ind,id=wgeesimdata$id,candidate.sets=NULL,name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=TRUE)
+output2<-MLIC.wgee(x=wgeesimdata$x,y=(wgeesimdata$y)
+                             ,x_mis=wgeesimdata$x_mis,r=wgeesimdata$obs_ind,id=wgeesimdata$id,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=TRUE)
 test_that("output equal: same output given both index and var.names, given joints=true",{expect_equal(output1,output2)})
 
 
-output1<-MLIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
-                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=list(c(1,2,3)),name.var.sets=NULL,dist="binomial",candidate.cor.sets="exchangeable",joints=TRUE)
-output2<-MLIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
-                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=list(c(1,2,3)),name.var.sets=NULL,dist="binomial",candidate.cor.sets="exchangeable",joints=TRUE)
+output1<-MLIC.wgee(x=wgeesimdata$x,y=(wgeesimdata$y)
+                             ,x_mis=wgeesimdata$x_mis,r=wgeesimdata$obs_ind,id=wgeesimdata$id,candidate.sets=list(c(1,2,3)),name.var.sets=NULL,dist="binomial",candidate.cor.sets="exchangeable",joints=TRUE)
+output2<-MLIC.wgee(x=wgeesimdata$x,y=(wgeesimdata$y)
+                             ,x_mis=wgeesimdata$x_mis,r=wgeesimdata$obs_ind,id=wgeesimdata$id,candidate.sets=list(c(1,2,3)),name.var.sets=NULL,dist="binomial",candidate.cor.sets="exchangeable",joints=TRUE)
 test_that("output equal: same output given both index and var.names, given joints=true",{expect_equal(output1,output2)})
 
 
-output1<-MLIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
-                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=NULL,name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
-output2<-MLIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
-                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
+output1<-MLIC.wgee(x=wgeesimdata$x,y=(wgeesimdata$y)
+                             ,x_mis=wgeesimdata$x_mis,r=wgeesimdata$obs_ind,id=wgeesimdata$id,candidate.sets=NULL,name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
+output2<-MLIC.wgee(x=wgeesimdata$x,y=(wgeesimdata$y)
+                             ,x_mis=wgeesimdata$x_mis,r=wgeesimdata$obs_ind,id=wgeesimdata$id,candidate.sets=list(c(1,2,3)),name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
 test_that("output equal: same output given both index and var.names, given joints=false",{expect_equal(output1,output2)})
 
 
-output1<-MLIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
-                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=NULL,name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="ar1",joints=FALSE)
-output2<-MLIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
-                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=NULL,name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets=c("ar1","exchangeable"),joints=FALSE)
+output1<-MLIC.wgee(x=wgeesimdata$x,y=(wgeesimdata$y)
+                             ,x_mis=wgeesimdata$x_mis,r=wgeesimdata$obs_ind,id=wgeesimdata$id,candidate.sets=NULL,name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets="ar1",joints=FALSE)
+output2<-MLIC.wgee(x=wgeesimdata$x,y=(wgeesimdata$y)
+                             ,x_mis=wgeesimdata$x_mis,r=wgeesimdata$obs_ind,id=wgeesimdata$id,candidate.sets=NULL,name.var.sets=list(c("intercept","x1","x2")),dist="binomial",candidate.cor.sets=c("ar1","exchangeable"),joints=FALSE)
 test_that("output equal: same output given multiple correlation structures, given joints=false",{expect_equal(output1,output2)})
 
 
-output1<-MLIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
-                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=list(c(1,2,3)),name.var.sets=NULL,dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
-output2<-MLIC.wgee(x=wgeetoydata$x,y=(wgeetoydata$y)
-                             ,x_mis=wgeetoydata$x_mis,r=wgeetoydata$obs_ind,id=wgeetoydata$id,candidate.sets=list(c(1,2,3)),name.var.sets=NULL,dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
+output1<-MLIC.wgee(x=wgeesimdata$x,y=(wgeesimdata$y)
+                             ,x_mis=wgeesimdata$x_mis,r=wgeesimdata$obs_ind,id=wgeesimdata$id,candidate.sets=list(c(1,2,3)),name.var.sets=NULL,dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
+output2<-MLIC.wgee(x=wgeesimdata$x,y=(wgeesimdata$y)
+                             ,x_mis=wgeesimdata$x_mis,r=wgeesimdata$obs_ind,id=wgeesimdata$id,candidate.sets=list(c(1,2,3)),name.var.sets=NULL,dist="binomial",candidate.cor.sets="exchangeable",joints=FALSE)
 test_that("output equal: same output given both index and var.names, given joints=false",{expect_equal(output1,output2)})
