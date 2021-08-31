6b9d26e60d7c3703ae69fd677c059bf756644f87
chencxxy28 <chencxxy28@gmail.com>
2021-08-31 08:41:38 -0400

    Update data_glmsimdata.R

--- R/data_glmsimdata.R
+++ R/data_glmsimdata.R
@@ -5,7 +5,7 @@
 #'
 #' @usage data(glmsimdata)
 #'
-#' @format An object of class \code{"data.frame"}
+#' @format An object of class \code{"list"}
 #' \describe{
 #'  \item{y}{The outcome generated from Negative Binomial distribution with the dispersion parameter parameter=2}
 #'  \item{x}{A covariate matrix, of which the first column are all ones and rest columns contain normally distributed
