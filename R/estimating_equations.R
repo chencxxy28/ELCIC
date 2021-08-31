19a23ab6feff6bb66630ea51124d3e9489272865
chencxxy28 <chencxxy28@gmail.com>
2021-08-30 20:04:37 -0400

    Update estimating_equations.R

--- R/estimating_equations.R
+++ R/estimating_equations.R
@@ -2,10 +2,10 @@
 
 #'@title Estimating equation for ELCIC under GLM
 #'@description A specified estimating equation for ELCIC under GLM. This estimating equation is used for marginal mean selection.
-#'@usage ee.glm(x, y, betahat, dist)
+#'@usage ee.glm(x, y, beta, dist)
 #'@param x A matrix containing covariates. The first column should be all ones corresponding to the intercept. See more details in
 #'@param y A vector containing outcomes.
-#'@param betahat A plug-in estimator solved by an external estimating procedure.
+#'@param beta A plug-in estimator solved by an external estimating procedure.
 #'@param dist A specified distribution. It can be "gaussian", "poisson",and "binomial".
 #'
 #'@return A matrix containing values of calculated estimating equations.
@@ -18,16 +18,16 @@
 #'data(glmsimdata)
 #'x<-glmsimdata$x
 #'y<-glmsimdata$y
-#'# obtain the estimates
+#'# obtain the estimates. Note that x matrix already contains intercept.
 #'fit<-glm(y~x-1,family="poisson")
-#'betahat<-fit$coefficients
-#'ee.matrix<-ee.glm(x, y, betahat, dist="poisson")
+#'beta<-fit$coefficients
+#'ee.matrix<-ee.glm(x, y, beta, dist="poisson")
 #'apply(ee.matrix,1,mean)
 #'
 #'@export
 
 ######for cross-sectional glm
-ee.glm<-function (x,y,betahat,dist)
+ee.glm<-function (x,y,beta,dist)
 {
     full<-ncol(x)
     samplesize<-nrow(x)
@@ -36,17 +36,17 @@ ee.glm<-function (x,y,betahat,dist)
     switch(dist,
            "gaussian"={    for (i in 1:samplesize)
            {
-               ee[,i]<-as.matrix(x[i,],ncol=1)%*%(y[i]-(t(as.matrix(x[i,],ncol=1))%*%betahat))
+               ee[,i]<-as.matrix(x[i,],ncol=1)%*%(y[i]-(t(as.matrix(x[i,],ncol=1))%*%beta))
            }
            },
            "binomial"={ for (i in 1:samplesize)
            {
-               ee[,i]<-as.matrix(x[i,],ncol=1)%*%(y[i]-(1+exp(-t(as.matrix(x[i,],ncol=1))%*%betahat))^(-1))
+               ee[,i]<-as.matrix(x[i,],ncol=1)%*%(y[i]-(1+exp(-t(as.matrix(x[i,],ncol=1))%*%beta))^(-1))
            }
            },
            "poisson"={for (i in 1:samplesize)
            {
-               ee[,i]<-as.matrix(x[i,],ncol=1)%*%(y[i]-exp(t(as.matrix(x[i,],ncol=1))%*%betahat))
+               ee[,i]<-as.matrix(x[i,],ncol=1)%*%(y[i]-exp(t(as.matrix(x[i,],ncol=1))%*%beta))
            }
            },
            stop("Invalid type of dist. It should be one of gaussian,binomial,poisson")
@@ -58,19 +58,19 @@ ee.glm<-function (x,y,betahat,dist)
     # {
     # for (i in 1:samplesize)
     # {
-    #     ee[,i]<-as.matrix(x[i,],ncol=1)%*%(y[i]-(t(as.matrix(x[i,],ncol=1))%*%betahat))
+    #     ee[,i]<-as.matrix(x[i,],ncol=1)%*%(y[i]-(t(as.matrix(x[i,],ncol=1))%*%beta))
     # }
     # }else if (dist=="poisson")
     # {
     #     for (i in 1:samplesize)
     #     {
-    #     ee[,i]<-as.matrix(x[i,],ncol=1)%*%(y[i]-exp(t(as.matrix(x[i,],ncol=1))%*%betahat))
+    #     ee[,i]<-as.matrix(x[i,],ncol=1)%*%(y[i]-exp(t(as.matrix(x[i,],ncol=1))%*%beta))
     #     }
     # }else if (dist=="binomial")
     # {
     #     for (i in 1:samplesize)
     #     {
-    #     ee[,i]<-as.matrix(x[i,],ncol=1)%*%(y[i]-(1+exp(-t(as.matrix(x[i,],ncol=1))%*%betahat))^(-1))
+    #     ee[,i]<-as.matrix(x[i,],ncol=1)%*%(y[i]-(1+exp(-t(as.matrix(x[i,],ncol=1))%*%beta))^(-1))
     #     }
     # }
 
@@ -89,7 +89,7 @@ ee.glm<-function (x,y,betahat,dist)
 #'@param y A vector containing outcomes.
 #'@param r A vector indicating the observation of outcomes: 1 for observed records, and 0 for unobserved records. The default setup is that all data are observed. See more in details section.
 #'@param id A vector indicating subject id.
-#'@param beta A plug-in estimator solved by an external estimation procedure, such as GEE.
+#'@param beta A plug-in estimator solved by an external estimation procedure.
 #'@param rho A correlation coefficients obtained from an external estimation procedure, such as GEE.
 #'@param phi An over-dispersion parameter obtained from an external estimation procedure, such as GEE.
 #'@param dist A specified distribution. It can be "gaussian", "poisson",and "binomial".
@@ -178,8 +178,8 @@ ee.gee<-function(y,x,r,id,beta,rho,phi,dist,corstr)
 
 
 
-#'@title Estimating equation for weighted GEE (WGEE) with data missing at random
-#'@description Calculate estimating equation from WGEE in ELCIC with data missing at random. This estimating equation is used for joint selection of marginal mean and working correlation structure.
+#'@title Estimating equation for weighted GEE (WGEE) for missing longitudinal data under the mechanism of missing at random and drop-out
+#'@description Calculate estimating equation from WGEE for missing longitudinal data under the mechanism of missing at random and drop-out. This estimating equation is used for joint selection of marginal mean and "working" correlation structure.
 #'@usage ee.wgee(y,x,r,pi,id,time,beta,rho,phi,dist,corstr)
 #'@param x A matrix containing covariates. The first column should be all ones corresponding to the intercept.
 #'@param y A vector containing outcomes. use NA to indicate missing outcomes.
@@ -204,7 +204,8 @@ ee.gee<-function(y,x,r,id,beta,rho,phi,dist,corstr)
 #'corstr<-"exchangeable"
 #'dist<-"binomial"
 #'id<-data_wgee$id
-#'# obtain the estimates
+#'# obtain the estimates.
+#'# Note that "obs_ind" is an indicator of observations in the missing data model.
 #'fit<-wgee(y~x1+x2+x3,data_wgee,id,family=dist,corstr =corstr,
 #'      scale = NULL,mismodel =obs_ind~x_mis1)
 #'beta<-as.vector(summary(fit)$beta)
@@ -269,7 +270,7 @@ ee.wgee<-function(y,x,r,pi,id,time,beta,rho,phi,dist,corstr)
 #######################################################
 ###### only for marginal mean selection
 
-#'@title Estimating equation of marginal mean for GEE without missing
+#'@title Estimating equation of marginal mean for GEE without missingness or missing completely at random
 #'@description Calculate estimating equation from GEE in ELCIC. This estimating equation is used for marginal mean selection.
 #'@usage ee.gee.mean(y,x,r,id,beta,rho,phi,dist,corstr)
 #'@param x A matrix containing covariates. The first column should be all ones corresponding to the intercept.
@@ -352,7 +353,7 @@ ee.gee.mean<-function(y,x,r,id,beta,rho,phi,dist,corstr)
 
 
 
-#'@title Estimating equation for marginal mean under weighted GEE(WGEE) with data missing at random
+#'@title Estimating equation for marginal mean under WGEE for missing longitudinal data under the mechanism of missing at random and drop-out
 #'@description Calculate estimating equation from WGEE in ELCIC. This estimating equation is used for marginal mean selection.
 #'@usage ee.wgee.mean(y,x,r,pi,id,time,beta,rho,phi,dist,corstr)
 #'@param x A matrix containing covariates. The first column should be all ones corresponding to the intercept.
@@ -380,7 +381,8 @@ ee.gee.mean<-function(y,x,r,id,beta,rho,phi,dist,corstr)
 #'corstr<-"exchangeable"
 #'dist<-"binomial"
 #'id<-data_wgee$id
-#'# obtain the estimates
+#'# obtain the estimates.
+#'# Note that "obs_ind" is an indicator of observations in the missing data model.
 #'fit<-wgee(y~x1+x2+x3,data_wgee,id,family=dist,corstr =corstr,
 #'      scale = NULL,mismodel =obs_ind~x_mis1)
 #'beta<-as.vector(summary(fit)$beta)
